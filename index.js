988da4994950cc1439ea03cb9fc54bb4479e072c
mnzvr <mansurjah@gmail.com>
2022-04-13 05:36:04 -0400

    2nd day

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.getElementbyID("count-el").innerText = 5
+// document.getElementbyID("count").innerText = 5
+
+// let count = 0;
+
+// let myAge = 24;
+// console.log(myAge);
+
+// let firstBatch = 5
+
+// let count = firstBatch
+
+// console.log(count)
+
